4cb37b388d0b1e2924feeccd5a806f835ddba5da
Abdu201213 <sabdu9049@gamil.com>
2025-04-06 07:22:46 +0300

    insertion sort implementation

---
+++ insertion.py
@@ -0,0 +1,17 @@
+class Insertion:
+    def __init__(self):
+        pass
+    def insert(self,data):
+        l = len(data)
+        for i in range(1,l):
+            key = data[i]
+            j = i-1
+            while j >=0 and key < data[j]:
+                data[j+1] = data[j]
+                j = j-1                
+            data[j+1] = key
+        return data    
+i = Insertion()
+a = [6,1,7,3,10,8]
+print("original array:",a)
+print("sorted array:",i.insert(a))    
